c6c49709f9c8afd38d1f3a9a4722bf5aa7577585
Leoluca D'Atri <22421051+Rithari@users.noreply.github.com>
2023-03-27 23:27:01 +0200

    [TEMP] Modifies main_ex1 and MergeBinSorter to find the optimal k value as described in the report.

--- Ex1/source/MergeBinSorter.c
+++ Ex1/source/MergeBinSorter.c
@@ -1,18 +1,20 @@
 #include "../headers/MergeBinSorter.h"
 
-void binary_insertion_sort(void *base, size_t nitems, size_t size, int (*compar)(const void *, const void *)) {
+void binary_insertion_sort(void *base, size_t nitems, size_t size, size_t *comparison_count, size_t *data_move_count, int (*compar)(const void *, const void *, size_t *)) {
     void *temp = malloc(size);
 
     for (size_t i = 1; i < nitems; i++) {
         void *key = (char *)base + i * size;
         memcpy(temp, key, size);
+        (*data_move_count)++;
 
         size_t low = 0;
         size_t high = i;
 
         while (low < high) {
             size_t mid = (low + high) / 2;
-            if (compar(temp, (char *)base + mid * size) < 0) {
+            (*comparison_count)++;
+            if (compar(temp, (char *)base + mid * size, comparison_count) < 0) {
                 high = mid;
             } else {
                 low = mid + 1;
@@ -20,31 +22,38 @@ void binary_insertion_sort(void *base, size_t nitems, size_t size, int (*compar)
         }
 
         memmove((char *)base + (low + 1) * size, (char *)base + low * size, (i - low) * size);
+        (*data_move_count) += (i - low);
         memcpy((char *)base + low * size, temp, size);
+        (*data_move_count)++;
     }
 
     free(temp);
 }
 
-
-
-void merge(void *base, void *temp, size_t left, size_t middle, size_t right, size_t size, int (*compar)(const void *, const void *)) {
+void merge(void *base, void *temp, size_t left, size_t middle, size_t right, size_t size, size_t *comparison_count, size_t *data_move_count, int (*compar)(const void *, const void *, size_t *)) {
+    if (middle < right && compar((char *)base + middle * size, (char *)base + (middle + 1) * size, comparison_count) <= 0) {
+        return;
+    }
     size_t left_size = middle - left + 1;
     size_t right_size = right - middle;
 
     memcpy(temp, (char *)base + left * size, left_size * size);
+    (*data_move_count) += left_size;
     memcpy((char *)temp + left_size * size, (char *)base + (middle + 1) * size, right_size * size);
+    (*data_move_count) += right_size;
 
     size_t i = 0;
     size_t j = left_size;
     size_t k = left;
 
     while (i < left_size && j < left_size + right_size) {
-        if (compar((char *)temp + i * size, (char *)temp + j * size) <= 0) {
+        if (compar((char *)temp + i * size, (char *)temp + j * size, comparison_count) <= 0) {
             memcpy((char *)base + k * size, (char *)temp + i * size, size);
+            (*data_move_count)++;
             i++;
         } else {
             memcpy((char *)base + k * size, (char *)temp + j * size, size);
+            (*data_move_count)++;
             j++;
         }
         k++;
@@ -52,63 +61,72 @@ void merge(void *base, void *temp, size_t left, size_t middle, size_t right, siz
 
     while (i < left_size) {
         memcpy((char *)base + k * size, (char *)temp + i * size, size);
+        (*data_move_count)++;
         i++;
         k++;
     }
 
     while (j < left_size + right_size) {
         memcpy((char *)base + k * size, (char *)temp + j * size, size);
+        (*data_move_count)++;
         j++;
         k++;
     }
 }
 
-void merge_sort_recursive(void *base, void *temp, size_t left, size_t right, size_t size, int (*compar)(const void *, const void *)) {
+void merge_sort_recursive(void *base, void *temp, size_t left, size_t right, size_t size, size_t *comparison_count, size_t *data_move_count, int (*compar)(const void *, const void *, size_t *)) {
     if (left >= right) {
         return;
     }
 
     size_t middle = left + (right - left) / 2;
-    merge_sort_recursive(base, temp, left, middle, size, compar);
-    merge_sort_recursive(base, temp, middle + 1, right, size, compar);
-    merge(base, temp, left, middle, right, size, compar);
+    merge_sort_recursive(base, temp, left, middle, size, comparison_count, data_move_count, compar);
+    merge_sort_recursive(base, temp, middle + 1, right, size, comparison_count, data_move_count, compar);
+    merge(base, temp, left, middle, right, size, comparison_count, data_move_count, compar);
 }
 
-
-void merge_sort(void *base, size_t nitems, size_t size, int (*compar)(const void *, const void *)) {
+void merge_sort(void *base, size_t nitems, size_t size, size_t *comparison_count, size_t *data_move_count, int (*compar)(const void *, const void *, size_t *)) {
     if (nitems <= 1) {
         return;
     }
 
     void *temp = malloc(nitems * size);
-    merge_sort_recursive(base, temp, 0, nitems - 1, size, compar);
+    merge_sort_recursive(base, temp, 0, nitems - 1, size, comparison_count, data_move_count, compar);
     free(temp);
 }
 
-
-void merge_binary_insertion_sort(void *base, size_t nitems, size_t size, size_t k, int (*compar)(const void *, const void *)) {
+void merge_binary_insertion_sort(void *base, size_t nitems, size_t size, size_t k, size_t *comparison_count, size_t *data_move_count, int (*compar)(const void *, const void *, size_t *)) {
     if (k == 0) {
-        merge_sort(base, nitems, size, compar);
+        merge_sort(base, nitems, size, comparison_count, data_move_count, compar);
         return;
     }
 
     if (nitems <= k) {
-        binary_insertion_sort(base, nitems, size, compar);
+        binary_insertion_sort(base, nitems, size, comparison_count, data_move_count, compar);
         return;
     }
 
-    // Sort subarrays
-    for (size_t i = 0; i < nitems; i += k) {
-        size_t subarray_size = (i + k > nitems) ? (nitems - i) : k;
-
-        if (subarray_size <= k) {
-            binary_insertion_sort((char *)base + i * size, subarray_size, size, compar);
-        } else {
-            merge_sort((char *)base + i * size, subarray_size, size, compar);
-        }
-    }
-
     void *temp = malloc(nitems * size);
+
+    size_t run_start = 0;
+    size_t run_end;
+
+    while (run_start < nitems) {
+        run_end = run_start + 1;
+        while (run_end < nitems && compar((char *)base + (run_end - 1) * size, (char *)base + run_end * size, comparison_count) <= 0) {
+            run_end++;
+        }
+
+        size_t run_size = run_end - run_start;
+
+        if (run_size <= k) {
+            binary_insertion_sort((char *)base + run_start * size, run_size, size, comparison_count, data_move_count, compar);
+        } else {
+            merge_sort((char *)base + run_start * size, run_size, size, comparison_count, data_move_count, compar);
+        }
+
+        run_start = run_end;
+    }
 
     for (size_t width = k; width < nitems; width *= 2) {
         for (size_t i = 0; i < nitems; i += 2 * width) {
@@ -125,7 +143,7 @@ void merge_binary_insertion_sort(void *base, size_t nitems, size_t size, size_t 
             }
 
             if (left < nitems && middle < nitems && right < nitems) {
-                merge(base, temp, left, middle, right, size, compar);
+                merge(base, temp, left, middle, right, size, comparison_count, data_move_count, compar);
             }
         }
     }
--- Ex1/source/main_ex1.c
+++ Ex1/source/main_ex1.c
@@ -1,101 +1,166 @@
-#include <time.h>
 #include <sys/times.h>
-#include <unistd.h>
 #include <stdbool.h>
+#include <float.h>
+#include <time.h>
 #include "../headers/main_ex1.h"
 
-int compare_name(const void *a, const void *b) {
+double get_time_diff(struct timespec start, struct timespec end) {
+    double elapsed = (end.tv_sec - start.tv_sec) * 1e9;
+    elapsed += (end.tv_nsec - start.tv_nsec);
+    return elapsed * 1e-9;
+}
+
+int compare_field1(const void *a, const void *b, size_t *comparison_count) {
+    (*comparison_count)++;
     const struct Row *row_a = (const struct Row *)a;
     const struct Row *row_b = (const struct Row *)b;
 
-    return strcmp(row_a->Name, row_b->Name);
+    return strcmp(row_a->field1, row_b->field1);
 }
 
-int compare_number1(const void *a, const void *b) {
+int compare_field2(const void *a, const void *b, size_t *comparison_count) {
+    (*comparison_count)++;
     const struct Row *row_a = (const struct Row *)a;
     const struct Row *row_b = (const struct Row *)b;
 
-    return row_a->Number1 - row_b->Number1;
+    return row_a->field2 - row_b->field2;
 }
 
-int compare_number2(const void *a, const void *b) {
+int compare_field3(const void *a, const void *b, size_t *comparison_count) {
+    (*comparison_count)++;
     const struct Row *row_a = (const struct Row *)a;
     const struct Row *row_b = (const struct Row *)b;
 
-    if (row_a->Number2 < row_b->Number2) {
-        return -1;
-    } else if (row_a->Number2 > row_b->Number2) {
-        return 1;
-    } else {
-        return 0;
-    }
+    return (row_a->field3 < row_b->field3) ? -1 : (row_a->field3 > row_b->field3) ? 1 : 0;
 }
 
-int compare(const void *a, const void *b) {
-    const struct Row *row_a = (const struct Row *)a;
-    const struct Row *row_b = (const struct Row *)b;
-
-    int name_cmp = strcmp(row_a->Name, row_b->Name);
-
-    if (name_cmp == 0) {
-        int number1_cmp = row_a->Number1 - row_b->Number1;
-        if (number1_cmp == 0) {
-            if (row_a->Number2 < row_b->Number2) {
-                return -1;
-            } else if (row_a->Number2 > row_b->Number2) {
-                return 1;
-            } else {
-                return 0;
-            }
-        } else {
-            return number1_cmp;
-        }
-    } else {
-        return name_cmp;
-    }
-}
-
-
-int main(int argc, char *argv[]) {
-    if (argc < 2) {
-        printf("Usage: %s <path_to_csv_file>\n", argv[0]);
-        return 1;
-    }
-
-    char *filename = argv[1];
-
-    struct Row *rows;
+void sort_records(const char *infile, const char *outfile, size_t k, size_t field) {
     int num_rows;
-
-    rows = read_csv(filename, &num_rows, false); // Set the has_header parameter to false if there's no header row
+    struct Row *rows = read_csv(infile, &num_rows, false);
     if (rows == NULL) {
-        perror("Failed to read CSV file");
+        printf("Failed to read the CSV file.\n");
         exit(1);
     }
 
-    // Time the sorting algorithm
-    struct tms start, end;
-    times(&start);
-    merge_binary_insertion_sort(rows, num_rows, sizeof(struct Row), 42, compare);
-    //qsort(rows, num_rows, sizeof(struct Row), compare_field);
-    times(&end);
-    printf("Finished sorting in %f seconds\n", (double) (end.tms_utime - start.tms_utime) / sysconf(_SC_CLK_TCK));
+    int (*compare_function)(const void *, const void *, size_t *);
 
-    // Save the sorted rows back to a new CSV file
-    char *output_filename = "../sorted.csv";
-    if (write_csv(output_filename, rows, num_rows)) {
-        perror("Failed to write sorted data to file");
-        exit(1);
+    switch (field) {
+        case 1:
+            compare_function = compare_field1;
+            break;
+        case 2:
+            compare_function = compare_field2;
+            break;
+        case 3:
+            compare_function = compare_field3;
+            break;
+        default:
+            printf("Invalid field value. Must be 1, 2, or 3.\n");
+            exit(1);
     }
 
-    printf("Sorted data written to file '%s'\n", output_filename);
+    int best_k_time = 0;
+    double best_time = DBL_MAX;
+
+    int best_k_operations = 0;
+    size_t best_operations = SIZE_MAX;
+
+    int best_k_data_moves = 0;
+    size_t best_data_moves = SIZE_MAX;
+
+    int best_k_overall = 0;
+    double best_score = DBL_MAX;
+
+    // Define the weights for the criteria
+    double time_weight = 1.0;
+    double operations_weight = 1.0;
+    double data_moves_weight = 1.0;
+
+    int min_k = 1;
+    int max_k = 100;
+
+    for (int current_k = min_k; current_k <= max_k; current_k++) {
+        struct Row *rows_copy = malloc(num_rows * sizeof(struct Row));
+        memcpy(rows_copy, rows, num_rows * sizeof(struct Row));
+
+        struct timespec start, end;
+        clock_gettime(CLOCK_MONOTONIC, &start);
+
+        size_t comparison_count = 0;
+        size_t data_move_count = 0;
+        merge_binary_insertion_sort(rows_copy, num_rows, sizeof(struct Row), current_k, &comparison_count, &data_move_count, compare_function);
+
+        clock_gettime(CLOCK_MONOTONIC, &end);
+        double current_time = get_time_diff(start, end);
+
+        printf("Finished sorting with k=%d, comparison_count=%zu, data_move_count=%zu in %f seconds\n", current_k, comparison_count, data_move_count, current_time);
+
+        if (current_time < best_time) {
+            best_time = current_time;
+            best_k_time = current_k;
+        }
+
+        if (comparison_count < best_operations) {
+            best_operations = comparison_count;
+            best_k_operations = current_k;
+        }
+
+        if (data_move_count < best_data_moves) {
+            best_data_moves = data_move_count;
+            best_k_data_moves = current_k;
+        }
+
+        double current_score = time_weight * current_time + operations_weight * comparison_count + data_moves_weight * data_move_count;
+
+        if (current_score < best_score) {
+            best_score = current_score;
+            best_k_overall = current_k;
+        }
+
+        free(rows_copy);
+    }
+
+    printf("Optimal k value for time: %d, with a sorting time of %f seconds\n", best_k_time, best_time);
+    printf("Optimal k value for operations: %d, with a count of %zu comparisons\n", best_k_operations, best_operations);
+    printf("Optimal k value for data moves: %d, with a count of %zu data moves\n", best_k_data_moves, best_data_moves);
+    printf("Optimal k value overall: %d, with a combined score of %f\n", best_k_overall, best_score);
+
+    // Sort and save the sorted rows with the best overall k value to a new CSV file
+    size_t comparison_count_final = 0;
+    size_t data_move_count_final = 0;
+    merge_binary_insertion_sort(rows, num_rows, sizeof(struct Row), best_k_overall, &comparison_count_final, &data_move_count_final, compare_function);
+    if (write_csv(outfile, rows, num_rows) != 0) {
+        perror("Failed to write CSV file");
+        exit(1);
+    }
+    printf("Sorted CSV file saved to '%s'\n", outfile);
 
     // Free the memory allocated for the rows array
     for (int i = 0; i < num_rows; i++) {
         free_row(&rows[i]);
     }
-
     free(rows);
+}
+
+
+int main(int argc, char *argv[]) {
+    if (argc < 5) {
+        printf("Usage: %s <path_to_csv_file> <output_csv_file> <k> <field>\n", argv[0]);
+        return 1;
+    }
+
+    char *infile = argv[1];
+    char *outfile = argv[2];
+    size_t k = atoi(argv[3]);
+    size_t field = atoi(argv[4]);
+
+    if (strcmp(infile, outfile) == 0) {
+        printf("Input and output files must be different.\n");
+        return 1;
+    }
+
+    sort_records(infile, outfile, k, field);
 
     return 0;
 }
+
