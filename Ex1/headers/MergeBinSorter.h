ed045d8cb7a04db153ef5cf043f617de9efc2188
Leoluca D'Atri <22421051+Rithari@users.noreply.github.com>
2023-05-10 10:55:53 +0200

    Rewrites Ex1 to respect time complexity

--- Ex1/headers/MergeBinSorter.h
+++ Ex1/headers/MergeBinSorter.h
@@ -9,13 +9,9 @@
 void binary_insertion_sort(void *base, size_t nitems, size_t size, int (*compar)(const void *, const void *));
 
 // Merge function declaration, used in merge sort algorithm
-void merge(void *base, void *temp, size_t left, size_t middle, size_t right, size_t size, size_t z, int (*compar)(const void *, const void *));
-
-// Merge sort recursive function declaration, used to implement the merge sort algorithm
-void merge_sort_recursive(void *base, void *temp, size_t left, size_t right, size_t size, size_t z, int (*compar)(const void *, const void *));
-
-// Merge sort function declaration
-void merge_sort(void *base, size_t nitems, size_t size, size_t k, int (*compar)(const void *, const void *));
+void merge(void *base, size_t split, size_t nitems, size_t size, int (*compar)(const void *, const void*));
+// Merge sort function declaration, used to implement the merge sort algorithm
+void merge_sort(void *base, void *temp, size_t left, size_t right, size_t size, size_t z, int (*compar)(const void *, const void *));
 
 // Merge-binary insertion sort function declaration, combining merge sort and binary insertion sort
 void merge_binary_insertion_sort(void *base, size_t nitems, size_t size, size_t k, int (*compar)(const void *, const void*));
--- Ex1/source/MergeBinSorter.c
+++ Ex1/source/MergeBinSorter.c
@@ -1,5 +1,22 @@
 #include "../headers/MergeBinSorter.h"
 
+
+int binary_search(void *elem, void *base, int nitems, int size, int (*compar)(const void *, const void*)) {
+    int low = 0;
+    int high = nitems;
+
+    while (low < high) {
+        size_t mid = low + (high - low) / 2;
+        if (compar((char *)base + mid * size, elem) < 0) {
+            low = mid + 1;
+        } else {
+            high = mid;
+        }
+    }
+
+    return low;
+}
+
 // Binary insertion sort function
 // Sorts an array using binary search to find the correct position for insertion
 // Params:
@@ -7,32 +24,16 @@
 // - nitems: number of elements in the array
 // - size: size of each element in bytes
 // - compar: comparison function to determine the order of the elements
-void binary_insertion_sort(void *base, size_t nitems, size_t size, int (*compar)(const void *, const void *)) {
-    void *temp = malloc(size);
+void binary_insertion_sort(void *base, size_t nitems, size_t size, int (*compar)(const void *, const void*)) {
 
-    for (size_t i = 1; i < nitems; i++) {
-        void *key = (char *)base + i * size;
-        memcpy(temp, key, size);
-
-        // Perform binary search to find the correct position for the key
-        size_t low = 0;
-        size_t high = i;
-
-        while (low < high) {
-            size_t mid = (low + high) / 2;
-            if (compar(temp, (char *)base + mid * size) < 0) {
-                high = mid;
-            } else {
-                low = mid + 1;
-            }
-        }
-
-        // Move elements to make space for the key at the correct position
-        memmove((char *)base + (low + 1) * size, (char *)base + low * size, (i - low) * size);
-        memcpy((char *)base + low * size, temp, size);
+    void *tmp = malloc(size);
+    for (size_t i = 1; i < nitems; ++i) {
+        size_t pos = binary_search((char *)base + i * size, base, i, size, compar);
+        memcpy(tmp, (char *)base + i * size, size);
+        memmove((char *)base + (pos + 1) * size, (char *)base + pos * size, (i - pos) * size);
+        memcpy((char *)base + pos * size, tmp, size);
     }
-
-    free(temp);
+    free(tmp);
 }
 
 // Merge function for the merge sort algorithm
@@ -46,50 +47,34 @@ void binary_insertion_sort(void *base, size_t nitems, size_t size, int (*compar)
 // - size: size of each element in bytes
 // - z: threshold (k) for using binary insertion sort for merging
 // - compar: comparison function to determine the order of the elements
-void merge(void *base, void *temp, size_t left, size_t middle, size_t right, size_t size, size_t z, int (*compar)(const void *, const void *)) {
-    // If the subarray size is less than or equal to z, use binary insertion sort
-    if(right - left + 1 <= z) {
-        binary_insertion_sort((char *)base + left * size, right - left + 1, size, compar);
-        return;
-    }
+void merge(void *base, size_t split, size_t nitems, size_t size, int (*compar)(const void *, const void*)) {
+    void* copy = malloc(nitems*size);
+    memcpy(copy, base, nitems*size);
+    char* L = (char*)copy;
+    char* Lend = L + split*size;
+    char* R = Lend;
+    char* Rend = L + nitems*size;
 
-    size_t left_size = middle - left + 1;
-    size_t right_size = right - middle;
-
-    // Copy the left and right subarrays into the temporary buffer
-    memcpy(temp, (char *)base + left * size, left_size * size);
-    memcpy((char *)temp + left_size * size, (char *)base + (middle + 1) * size, right_size * size);
-
-    // Merge the subarrays back into the main array
-    size_t i = 0;
-    size_t j = left_size;
-    size_t k = left;
-
-    while (i < left_size && j < left_size + right_size) {
-        if (compar((char *)temp + i * size, (char *)temp + j * size) <= 0) {
-            memcpy((char *)base + k * size, (char
-            *)temp + i * size, size);
-            i++;
+    char* p = (char*)base;
+    for (int i=0; i<nitems; ++i) {
+        if (L == Lend) {
+            memcpy(p, R, size);
+            R += size;
+        } else if (R == Rend) {
+            memcpy(p, L, size);
+            L += size;
+        } else if (compar(L, R) < 0) {
+            memcpy(p, L, size);
+            L += size;
         } else {
-            memcpy((char *)base + k * size, (char *)temp + j * size, size);
-            j++;
+            memcpy(p, R, size);
+            R += size;
         }
-        k++;
+        p += size;
     }
-
-    // Copy any remaining elements from the left and right subarrays
-    while (i < left_size) {
-        memcpy((char *)base + k * size, (char *)temp + i * size, size);
-        i++;
-        k++;
-    }
-
-    while (j < left_size + right_size) {
-        memcpy((char *)base + k * size, (char *)temp + j * size, size);
-        j++;
-        k++;
-    }
+    free(copy);
 }
+
 
 // Recursive merge sort function
 // Params:
@@ -100,36 +85,23 @@ void merge(void *base, void *temp, size_t left, size_t middle, size_t right, siz
 // - size: size of each element in bytes
 // - k: threshold for using binary insertion sort for merging
 // - compar: comparison function to determine the order of the elements
-void merge_sort_recursive(void *base, void *temp, size_t left, size_t right, size_t size, size_t k, int (*compar)(const void *, const void *)) {
+void merge_sort(void *base, void *temp, size_t left, size_t right, size_t size, size_t z, int (*compar)(const void *, const void *)) {
     if (left >= right) {
         return;
     }
 
     size_t middle = left + (right - left) / 2;
-    merge_sort_recursive(base, temp, left, middle, size, k, compar);
-    merge_sort_recursive(base, temp, middle + 1, right, size, k, compar);
+    merge_sort(base, temp, left, middle, size, z, compar);
+    merge_sort(base, temp, middle + 1, right, size, z, compar);
 
     // Merge the two subarrays only if they are out of order
-    if (compar((char *)base + middle * size, (char *)base + (middle + 1) * size) > 0) {
-        merge(base, temp, left, middle, right, size, k, compar);
+    void *middle_element = (char *)base + middle * size;
+    void *middle_plus_one_element = (char *)base + (middle + 1) * size;
+    if (compar(middle_element, middle_plus_one_element) > 0) {
+        size_t split = middle - left + 1;
+        size_t nitems = right - left + 1;
+        merge(base, split, nitems, size, compar);
     }
-}
-
-// Merge sort function
-// Params:
-// - base: pointer to the first element of the array
-// - nitems: number of elements in the array
-// - size: size of each element in bytes
-// - k: threshold for using binary insertion sort for merging
-// - compar: comparison function to determine the order of the elements
-void merge_sort(void *base, size_t nitems, size_t size, size_t k, int (*compar)(const void *, const void *)) {
-    if (nitems <= 1) {
-        return;
-    }
-
-    void *temp = malloc(nitems * size);
-    merge_sort_recursive(base, temp, 0, nitems - 1, size, k, compar);
-    free(temp);
 }
 
 // Merge sort combined with binary insertion sort
@@ -139,9 +111,8 @@ void merge_sort(void *base, size_t nitems, size_t size, size_t k, int (*compar)(
 // - size: size of each element in bytes
 // - k: threshold for using binary insertion sort for merging
 // - compar: comparison function to determine the order of the elements
-void merge_binary_insertion_sort(void *base, size_t nitems, size_t size, size_t k, int (*compar)(const void *, const void *)) {
-    if (k == 0) {
-        merge_sort(base, nitems, size, k, compar);
+void merge_binary_insertion_sort(void *base, size_t nitems, size_t size, size_t k, int (*compar)(const void *, const void*)) {
+    if (nitems <= 1) {
         return;
     }
 
@@ -150,34 +121,8 @@ void merge_binary_insertion_sort(void *base, size_t nitems, size_t size, size_t 
         return;
     }
 
-    void *temp = malloc(nitems * size);
-
-    // First, sort chunks of size 'k' using binary_insertion_sort
-    for (size_t i = 0; i < nitems; i += k) {
-        size_t chunk_size = (i + k < nitems) ? k : (nitems - i);
-        binary_insertion_sort((char *)base + i * size, chunk_size, size, compar);
-    }
-
-    // Then, merge the chunks together iteratively
-    for (size_t width = k; width < nitems; width *= 2) {
-        for (size_t i = 0; i < nitems; i += 2 * width) {
-            size_t left = i;
-            size_t middle = i + width - 1;
-            size_t right = i + 2 * width - 1;
-
-            if (middle >= nitems) {
-                middle = nitems - 1;
-            }
-
-            if (right >= nitems) {
-                right = nitems - 1;
-            }
-
-            if (left < nitems && middle < nitems && right < nitems) {
-                merge(base, temp, left, middle, right, size, k, compar);
-            }
-        }
-    }
-
-    free(temp);
+    size_t split = nitems / 2;
+    merge_binary_insertion_sort(base, split, size, k, compar);
+    merge_binary_insertion_sort(((char *)base) + split * size, nitems - split, size, k, compar);
+    merge(base, split, nitems, size, compar);
 }
